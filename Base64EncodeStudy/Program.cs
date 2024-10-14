ae889ba1be049bf5a899359b6fab12d06cdc6e43
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2024-10-14 15:31:33 +0900

    .NET 8 に移行。

--- Base64EncodeStudy/Program.cs
+++ Base64EncodeStudy/Program.cs
@@ -1,26 +1,25 @@
 ﻿using System;
 using System.Text;
 
-namespace Base64EncodeStudy
+namespace Base64EncodeStudy;
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string originalString = "あいうえお";
-            Console.WriteLine("元の文字列 : " + originalString);
+        string originalString = "あいうえお";
+        Console.WriteLine("元の文字列 : " + originalString);
 
-            byte[] originalByte = Encoding.UTF8.GetBytes(originalString);
-            Console.WriteLine("元のバイト文字列 : " + BitConverter.ToString(originalByte));
+        byte[] originalByte = Encoding.UTF8.GetBytes(originalString);
+        Console.WriteLine("元のバイト文字列 : " + BitConverter.ToString(originalByte));
 
-            string encodeString = Convert.ToBase64String(originalByte);
-            Console.WriteLine("エンコードした文字列 : " + encodeString);
+        string encodeString = Convert.ToBase64String(originalByte);
+        Console.WriteLine("エンコードした文字列 : " + encodeString);
 
-            byte[] decodeByte = Convert.FromBase64String(encodeString);
-            Console.WriteLine("デコードしたバイト配列 : " + BitConverter.ToString(decodeByte));
+        byte[] decodeByte = Convert.FromBase64String(encodeString);
+        Console.WriteLine("デコードしたバイト配列 : " + BitConverter.ToString(decodeByte));
 
-            string decodeString = Encoding.UTF8.GetString(decodeByte);
-            Console.WriteLine("デコードした文字列 : " + decodeString);
-        }
+        string decodeString = Encoding.UTF8.GetString(decodeByte);
+        Console.WriteLine("デコードした文字列 : " + decodeString);
     }
 }
